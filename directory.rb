93774d290561703bcf887a5a25f8efec1b5549a1
isabelcooper <isabel@triptease.com>
2018-12-19 11:10:54 +0000

    load file

--- directory.rb
+++ directory.rb
@@ -15,7 +15,9 @@ def process
   when 2
     show_students
   when 3
-    save_student
+    save_students
+  when 4
+    load_students
   when 9
     exit
   else
@@ -27,6 +29,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"
 end
 
@@ -98,7 +101,7 @@ def input_students
   end
 end
 
-def save_student
+def save_students
   #open the file in writing mode
   file = File.open("students.csv", "w")
   #iterate over array of students
@@ -106,6 +109,15 @@ def save_student
     student_data = [student[:name], student[:cohort], student[:country_of_birth]]
     csv_line = student_data.join(",")
     file.puts csv_line
+  end
+  file.close
+end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+  name, cohort, country_of_birth = line.chomp.split(",")
+    @students << {name: name, cohort: cohort.to_sym, country_of_birth: country_of_birth}
   end
   file.close
 end
